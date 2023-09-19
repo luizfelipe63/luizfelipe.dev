b8fa23a13dd854ea0db0794bc8563340415a4b68
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-09-19 09:43:41 -0300

    Feat: Creat component About

---
+++ src/components/About.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import AboutMe from '../assets/me-foto-principal.jpg'
+
+export function About() {
+  return (
+    <section className="mx-auto max-w-[1216px] pb-36 pt-20">
+      <div className="space-y-4 text-center">
+        <span className="tracking-[3px] text-gray-400">SOBRE MIM</span>
+        <h2 className="text-center text-5xl">Conheça minha história...</h2>
+      </div>
+      <div className="flex justify-around gap-8 pt-20">
+        <div className="w-[500px]">
+          <Image src={AboutMe} alt="" className="rounded-lg" />
+        </div>
+        <div className="max-w-lg space-y-4 text-gray-400">
+          <p>Meu nome é Jonas Milan e sou de Chapecó – SC (Brasil).</p>
+          <p>
+            Minha jornada no design começou em 2015, quando iniciei a faculdade
+            de Jogos Digitais, que foi onde obtive muitos conhecimentos nas mais
+            diversas áreas de criação.
+          </p>{' '}
+          <p>
+            Milan e sou de Chapecó – SC (Brasil). Minha jornada no design
+            começou em 2015, quando iniciei a faculdade de Jogos Digitais, que
+            foi onde obtive muitos conhecimentos nas mais diversas áreas de
+            criação…
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
