0e479970744890357c5694375c1c377f7a4ed4f9
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-07-11 15:19:21 -0300

    UX of the header completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
 import ProfileLogo from '../assets/mePraia.jpg'
 import { ArrowRight } from 'phosphor-react'
-import Link from 'next/link'
 import Image from 'next/image'
-import ilustacao from '../assets/illustration3.png'
 
 export function Header() {
   return (
@@ -41,31 +39,36 @@ export function Header() {
           </ul>
         </div>
       </nav>
-      <section className="pt-12 sm:pt-10 flex items-center m-auto max-w-[1120px]">
-        <div className="flex flex-col gap-8 w-full">
-          {/* <h1 className="w-max rounded-lg bg-gradient-hero px-6 py-3 text-6xl sm:text-3xl">
+      <section className="pt-20 flex m-auto max-w-[1120px]">
+        <div className="flex flex-col gap-11 items-center">
+          <h1 className="w-max rounded-lg bg-gradient-hero px-6 py-3 text-6xl sm:text-3xl">
             Prazer, Luiz Felipe
-          </h1> */}
-          <h2 className="text-5xl">Tenha seu negocio com uma presença online agora mesmo</h2>
-          <p className="text-gray-500">
+          </h1>
+          <h2 className="text-5xl sm:text-xl">Sou Desenvolvedor Full-Stack</h2>
+          <p className="max-w-3xl text-gray-500 text-center">
             Sou um profissional altamente qualificado. Com habilidades em React,
             Typescript, JavaScript, NextJS e Node. Assim trasnformo conceitos em
             realidade.{' '}
           </p>
-          <Link href="/solicitar-proposta">
-            <button className="flex items-center gap-2 rounded-lg bg-gray-50 px-6 py-4 text-black transition hover:bg-blue-400 hover:text-white">
-              SOLICITE SUA PROPOSTA
-              <ArrowRight size={24} />
-            </button>
-          </Link>
+         <div className='w-full border flex justify-between border-blue-400 py-8 px-12 rounded'>
+          <div className='flex flex-col justify-center text-center gap-2'>
+            <h1 className='text-2xl'>70%</h1>
+            <span className='text-gray-500'>Job actviments</span>
+          </div>
+          <div className='flex flex-col justify-center text-center gap-2'>
+            <h1 className='text-2xl'>1.6K</h1>
+            <span className='text-gray-500'>Job actviments</span>
+          </div>
+          <div className='flex flex-col justify-center text-center gap-2'>
+            <h1 className='text-2xl'>$10.4M</h1>
+            <span className='text-gray-500'>Job actviments</span>
+          </div>
+          <div className='flex flex-col justify-center text-center gap-2'>
+            <h1 className='text-2xl'>+24</h1>
+            <span className='text-gray-500'>Job actviments</span>
+          </div>
+         </div>
         </div>
-          <Image 
-          src={ilustacao} 
-          alt=''
-          className=''
-          width={600}
-          >
-          </Image>
       </section>
     </header>
   )
