a8476f39e6159cf060506773d5226b2593949c69
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-07-31 18:03:03 -0300

    Visual layout of the services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,4 @@
-import { Code, Database, StackSimple } from 'phosphor-react'
+import { Code, Database, Desktop, StackSimple } from 'phosphor-react'
 
 export function Services() {
   return (
@@ -11,49 +11,40 @@ export function Services() {
           Meus serviços prestados
         </span>
       </div>
-      <div className="grid grid-cols-2 gap-8 pt-20">
-        <div className="w-full border border-blue-500 px-4 py-8">
-          <div className="flex flex-col gap-3">
-            <StackSimple size={32} />
-            <h3 className="text-2xl">Design de interfaces web</h3>
-            <p className="text-sm text-gray-400">
+      <div className="flex pt-20 place-items-center gap-6">
+        <div className="bg-neutral-950 px-6 py-8 rounded">
+          <div className="flex flex-col gap-5 items-center">
+            <Desktop size={36} />
+            {/* <span className='border border-blue-500 w-14'></span> */}
+            <h3 className="text-2xl text-center">Design de interfaces</h3>
+            <p className="text-sm text-gray-400 text-center">
               Interfaces para websites e landing pages para qualquer nicho,
               focado em seu público e forte apelo visual
             </p>
           </div>
-        </div>
-        <div className="w-full border border-blue-500 px-4 py-8">
-          <div className="flex flex-col gap-3 ">
-            <Code size={32} />
-            <h3 className="text-2xl">
-              Desenvolvimento Front-End
-            </h3>
-            <p className="text-sm text-gray-400">
-              Desenvolvo interfaces que seguem rigorosamente aos padrões de
-              acessibilidade, responsividade e SEO.
+        </div>  
+        <div className="bg-neutral-950 px-6 py-8 rounded">
+          <div className="flex flex-col gap-5 items-center">
+            <StackSimple size={36} />
+            {/* <span className='border border-blue-500 w-14'></span> */}
+            <h3 className="text-2xl text-center ">Desenvolvimento</h3>
+            <p className="text-sm text-gray-400 text-center">
+              Interfaces para websites e landing pages para qualquer nicho,
+              focado em seu público e forte apelo visual
             </p>
           </div>
-        </div>
-        <div className="w-full border border-blue-500 px-4 py-8">
-          <div className="flex flex-col gap-3">
-            <Database size={32} />
-            <h3 className="text-2xl">Desenvolvimento Back-End</h3>
-            <p className="text-sm text-gray-400">
-              Construo arquiteturas escaláveis e implemento toda a lógica de
-              negócios, seguindo aos princípios de Clean Code, como SOLID e DDD.
+        </div>  
+        <div className="bg-neutral-950 px-6 py-8 rounded">
+          <div className="flex flex-col gap-5 items-center">
+            <StackSimple size={36} />
+            {/* <span className='border border-blue-500 w-14'></span> */}
+            <h3 className="text-2xl text-center">UI/UX Design</h3>
+            <p className="text-sm text-gray-400 text-center">
+              Interfaces para websites e landing pages para qualquer nicho,
+              focado em seu público e forte apelo visual
             </p>
           </div>
-        </div>
-        <div className="w-full border border-blue-500 px-4 py-8">
-          <div className="flex flex-col gap-3 ">
-            <Database size={32} />
-            <h3 className="text-2xl">Desenvolvimento Back-End</h3>
-            <p className="text-sm text-gray-400">
-              Construo arquiteturas escaláveis e implemento toda a lógica de
-              negócios, seguindo aos princípios de Clean Code, como SOLID e DDD.
-            </p>
-          </div>
-        </div>
+        </div>  
       </div>
     </section>
   )
