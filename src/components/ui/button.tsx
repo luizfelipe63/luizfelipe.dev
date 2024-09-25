b67b77529c25bc3823338118330b3e5c4141e6f7
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-09-25 13:24:15 -0300

    feat: button ui component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,41 @@
+import { forwardRef, type ComponentProps } from 'react'
+import { tv, type VariantProps } from 'tailwind-variants'
+
+const button = tv({
+  base: 'flex items-center gap-2 rounded-lg text-white',
+
+
+  variants: {
+    variant: {
+      primary:
+        'bg-blue-500 text-white transition hover:bg-white hover:text-black',
+      secondary: 'text-white border border-blue-400 transition hover:bg-blue-400 hover:border-1 hover:border-transparent',
+    },
+
+    size: {
+      default: 'px-6 py-4',
+      sm: 'px-3 py-1.5',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'default',
+  },
+})
+
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        className={button({ variant, size, className })}
+      />
+    )
+  }
+)
+
+Button.displayName = 'Button'
